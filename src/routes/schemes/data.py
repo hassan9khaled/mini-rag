da316cb73520a3472a9d6eb24adce900ea9f759e
hassan9khaled <hassan9khaled@gmail.com>
2025-07-08 19:17:19 +0300

    create a schemes folder and make a pydantic scheme to validate the processing requests

---
+++ src/routes/schemes/data.py
@@ -0,0 +1,9 @@
+from typing import Optional
+from pydantic import BaseModel
+
+
+class ProcessRequest(BaseModel):
+    file_id: str
+    chunk_size: Optional[int] = 100
+    overlab_size: Optional[int] = 20
+    do_reset: Optional[int] = 0
