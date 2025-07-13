4c8cea512b8394fae25462f9a4b7dcd42c688a96
hassan9khaled <hassan9khaled@gmail.com>
2025-07-13 12:38:29 +0300

    enhance the main file

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ async def lifespan(app: FastAPI):
     # Initialize MongoDB connection
     app.state.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
     app.state.db_client = app.state.mongo_conn[settings.MONGODB_DATABASE]
-    print("Connected to MongoDB!")
+    print("\x1b[36mConnected to MongoDB!\033[0m")
 
     # Setup the LLMFactory
     llm_provider_factory = LLMProviderFactory(settings)
@@ -23,20 +23,20 @@ async def lifespan(app: FastAPI):
     app.state.generation_client = llm_provider_factory.create(provider=settings.GENERATION_BACKEND)
     app.state.generation_client.set_generation_model(model_id=settings.GENERATION_MODEL_ID)
 
-    print(f"{settings.GENERATION_BACKEND} Generation Model loaded successfully!")
+    print(f"\x1b[36m{settings.GENERATION_BACKEND} Generation Model loaded successfully!\033[0m")
 
     # Embedding Client
     app.state.embedding_client = llm_provider_factory.create(provider=settings.EMBEDDING_BACKEND)
     app.state.embedding_client.set_embedding_model(model_id=settings.EMBEDDING_MODEL_ID,
                                                    embedding_size=settings.EMBEDDING_MODEL_SIZE)
 
-    print(f"{settings.EMBEDDING_BACKEND} Embedding Model loaded successfully!")
+    print(f"\x1b[36m{settings.EMBEDDING_BACKEND} Embedding Model loaded successfully!\033[0m")
 
     yield  # This separates startup from shutdown code
     
     # Shutdown code
     app.state.mongo_conn.close()
-    print("Closed MongoDB connection!")
+    print("\x1b[33mClosed MongoDB connection!\033[0m")
 
 app = FastAPI(lifespan=lifespan)
 
