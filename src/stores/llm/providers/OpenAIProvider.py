70e2b461a4854346ba04b16a4fc96e2c3bf8182d
hassan9khaled <hassan9khaled@gmail.com>
2025-07-16 10:31:53 +0300

    enhance the llms

--- src/stores/llm/providers/OpenAIProvider.py
+++ src/stores/llm/providers/OpenAIProvider.py
@@ -2,6 +2,7 @@ from ..LLMInterface import LLMInterface
 from ..LLMEnums import OpenAIEnums
 from openai import OpenAI
 import logging
+from typing import Union, List
 
 class OpenAIProvider(LLMInterface):
     
@@ -71,11 +72,14 @@ class OpenAIProvider(LLMInterface):
         
         return response.choices[0].message["content"]
 
-    def embed_text(self, text: str, document_type: str = None):
+    def embed_text(self, text: Union[str, List[str]], document_type: str = None):
 
         if not self.client:
             self.logger.error('OpenAI client was not set')
             return None
+        
+        if isinstance(text, str):
+            text = [text]
         
         if not self.embedding_model_id:
             self.logger.error('Embedding model for OpenAI was not set')
@@ -91,7 +95,10 @@ class OpenAIProvider(LLMInterface):
             self.logger.error('Error while embedding text with OpenAI')
             return None
         
-        return response.data[0].embedding
+        
+        embeds = response.data[0].embedding
+        
+        return embeds
     
     def construct_prompt(self, prompt, role):
 
